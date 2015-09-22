9ca9b59884886e7a7d8634f6e8b13acc4ecf9690
kristian94 <kristian.o.g.nielsen@gmail.com>
2015-09-22 11:46:24 +0200

    1234

--- TeamDemo/src/teamdemo/TeamDemo.java
+++ TeamDemo/src/teamdemo/TeamDemo.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -22,31 +20,3 @@ public class TeamDemo {
     }
     
 }
-=======
->>>>>>> origin/master
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package teamdemo;
-
-/**
- *
- * @author Kristian
- */
-public class TeamDemo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("sup yo");
-    }
-    
-}
-<<<<<<< HEAD
-=======
->>>>>>> origin/master
->>>>>>> origin/master
